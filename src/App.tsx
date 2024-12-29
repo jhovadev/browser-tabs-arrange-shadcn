5b196dea3967776cab1063bb26aa6c5d1b0bf7a6
Jhovadev <jorgearocutipa036@gmail.com>
2024-12-29 03:09:43 -0500

    added manifest json && useEffect to handle window-tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 // hooks
+import { useEffect, useState } from "react";
 
 import { useStoreTabs } from "./store";
 /* import reactLogo from "./assets/react.svg";
@@ -6,18 +7,52 @@ import viteLogo from "/vite.svg"; */
 
 // Components
 import { Toaster } from "@/components/ui/toaster";
-
+import { Tab } from "./columns";
 import { DataTable } from "./data-table";
 
 // Definition
 import { columns } from "./columns";
 
 function App() {
-  const { tabs } = useStoreTabs();
+  const { tabs, setTabs } = useStoreTabs();
+
+  useEffect(() => {
+    const fetchTabs = async () => {
+      try {
+        const windowInfo = await chrome.windows.getCurrent({
+          populate: true,
+          windowTypes: ["normal"],
+        });
+
+        if (windowInfo && windowInfo.tabs) {
+          // Ensure each tab has a unique id
+          const tabsWithIds = windowInfo.tabs.map(
+            (tab, index): Tab => ({
+              id: index, // Asignamos un id único (puede ser `index` u otro identificador)
+              title: tab.title,
+              favIconUrl: tab.favIconUrl,
+              url: tab.url,
+            }),
+          );
+          setTabs(tabsWithIds);
+          console.log(tabsWithIds);
+          /* const tabs: Tab[] = windowInfo.tabs; */
+          /* 
+          tabs.forEach(tab => {
+            const {title, favIconUrl, url} = tab;
+            // Aquí puedes realizar cualquier otra operación con los tabs
+          }); */
+        }
+      } catch (error) {
+        console.error("Error fetching tabs:", error);
+      }
+    };
+    fetchTabs();
+  }, []);
 
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-center gap-4 p-4 text-center">
+      <main className="flex w-[700px] min-h-[600px] overflow-hidden flex-col items-center justify-center gap-4 p-4 text-center">
         <DataTable
           columns={columns}
           data={tabs}
